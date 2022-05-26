9abb23973d12c74f6e7a93d1502c49017945330b
nfolkman <nathanielfolkman@gmail.com>
2022-05-25 23:43:17 -0500

    first attempt at server integration lol

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 // server file 
+
+const http = require('http')
+
+const fs = require('fs')
+
+http.createServer((req, res) => {
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(data)
+    res.end()
+  })
+}).listen(8000)
+
+
